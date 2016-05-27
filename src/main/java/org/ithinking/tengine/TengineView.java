caa4577427b6341b35d274152fc168ce76630040
fucj <fucj@hyxt.com>
2016-05-27 17:22:13 +0800

    完善

--- src/main/java/org/ithinking/tengine/TengineView.java
+++ src/main/java/org/ithinking/tengine/TengineView.java
@@ -1,5 +1,8 @@
 package org.ithinking.tengine;
 
+import org.ithinking.tengine.core.Context;
+import org.ithinking.tengine.core.Template;
+import org.ithinking.tengine.core.TemplateManager;
 import org.springframework.web.servlet.View;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,7 +12,16 @@ import java.util.Map;
 /**
  * @author agan
  */
-public class TengineView implements View{
+public class TengineView implements View {
+
+    private TemplateManager manager = null;
+    private Template template = null;
+
+    public TengineView(Template template, TemplateManager manager) {
+        this.manager = manager;
+        this.template = template;
+    }
+
     @Override
     public String getContentType() {
         return null;
@@ -17,6 +29,11 @@ public class TengineView implements View{
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        Context context = new HttpServletRequestContext(manager, request, response);
+        String path = request.getServletPath();
+        Template template = context.loadTemplate(path);
+        if (template != null) {
+            template.render(context);
+        }
     }
 }
--- src/main/java/org/ithinking/tengine/TengineViewResolver.java
+++ src/main/java/org/ithinking/tengine/TengineViewResolver.java
@@ -1,21 +1,46 @@
 package org.ithinking.tengine;
 
+import org.ithinking.tengine.core.Configuration;
+import org.ithinking.tengine.core.Loader;
+import org.ithinking.tengine.core.Template;
+import org.ithinking.tengine.core.TemplateManager;
+import org.ithinking.tengine.html.parser.HtmlParser;
+import org.ithinking.tengine.loader.ClasspathLoader;
+import org.springframework.web.context.support.WebApplicationObjectSupport;
 import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 
+import javax.servlet.ServletContext;
 import java.util.Locale;
 
 /**
  * @author fuchujian
  */
-public class TengineViewResolver extends org.springframework.web.servlet.view.AbstractCachingViewResolver implements org.springframework.core.Ordered{
+//  org.springframework.web.servlet.view.AbstractCachingViewResolver
+public class TengineViewResolver extends WebApplicationObjectSupport implements ViewResolver, org.springframework.core.Ordered {
+
+    private TemplateManager manager = null;
 
     @Override
-    protected View loadView(String viewName, Locale locale) throws Exception {
-        return null;
+    protected void initServletContext(ServletContext servletContext) {
+        super.initServletContext(servletContext);
+        Configuration conf = new Configuration();
+        Loader loader = new ClasspathLoader(conf);
+        HtmlParser parser = new HtmlParser();
+        manager = new TemplateManager(loader, conf, parser);
     }
 
     @Override
     public int getOrder() {
         return 0;
+    }
+
+    @Override
+    public View resolveViewName(String viewName, Locale locale) throws Exception {
+        Template template = manager.getTemplate(viewName);
+        if (template != null) {
+            return new TengineView(template, manager);
+        }
+        return null;
     }
 }
