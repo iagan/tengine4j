fbe165523204c0582f8fde6d5004907afe3ab027
fucj <fucj@hyxt.com>
2016-05-27 15:39:43 +0800

    完善

--- src/main/java/org/ithinking/tengine/core/Configuration.java
+++ src/main/java/org/ithinking/tengine/core/Configuration.java
@@ -1,5 +1,19 @@
 package org.ithinking.tengine.core;
 
-public class Configuration {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Configuration {
+    // 模板编码
+    private String charset = "UTF-8";
+    // tg.view.prefix
+    private String viewPrefix = "/WEB-INF/views/";
+    // tg.view.suffix= .html
+    private String viewSuffix = ".html";
+
+    private static final Map<String,String> CONFIGS;
+
+    static {
+        CONFIGS = new HashMap<>();
+    }
 }
