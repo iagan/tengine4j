b50183d30bee4052d503829c3876d7eeaa589b3b
vajava@126.com <vajava@126.com>
2016-05-28 14:14:40 +0800

    完善

--- src/main/java/org/ithinking/tengine/loader/ClasspathLoader.java
+++ src/main/java/org/ithinking/tengine/loader/ClasspathLoader.java
@@ -8,38 +8,43 @@ import org.ithinking.tengine.core.Resource;
 
 public class ClasspathLoader extends AbstractLoader {
 
-	@SuppressWarnings("unused")
-	private Configuration conf;
+    @SuppressWarnings("unused")
+    private Configuration conf;
 
-	public ClasspathLoader(Configuration conf) {
-		this.conf = conf;
-	}
+    public ClasspathLoader(Configuration conf) {
+        this.conf = conf;
+    }
 
-	public Resource load(String templateId) {
-		ClassLoader classLoader = this.getClass().getClassLoader();
-		URL url = classLoader.getResource("");
-		Resource res = null;
-		if (url.getProtocol().equals("file")) {
-			File file = new File(url.getFile() + "/" + templateId);
-			String text = this.load(file);
-			res = new Resource();
-			res.setId(templateId);
-			res.setText(text);
-			res.setPath(file.getAbsolutePath());
-			res.setLastModified(file.lastModified());
-		}
-		return res;
-	}
+    public Resource load(String templateId) {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        URL url = classLoader.getResource("");
+        Resource res = null;
+        if (url.getProtocol().equals("file")) {
+            File file = new File(url.getFile() + "/" + templateId);
+            if (file.exists()) {
+                String text = this.load(file);
+                if (text != null) {
+                    res = new Resource();
+                    res.setId(templateId);
+                    res.setText(text);
+                    res.setPath(file.getAbsolutePath());
+                    res.setLastModified(file.lastModified());
+                }
+            }
 
-	@Override
-	public boolean isModified(String templateId, long lastModified) {
-		ClassLoader classLoader = this.getClass().getClassLoader();
-		URL url = classLoader.getResource("");
-		if (url.getProtocol().equals("file")) {
-			File file = new File(url.getFile() + "/" + templateId);
-			return file.lastModified() != lastModified;
-		}
-		return false;
-	}
+        }
+        return res;
+    }
+
+    @Override
+    public boolean isModified(String templateId, long lastModified) {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        URL url = classLoader.getResource("");
+        if (url.getProtocol().equals("file")) {
+            File file = new File(url.getFile() + "/" + templateId);
+            return file.lastModified() != lastModified;
+        }
+        return false;
+    }
 
 }
