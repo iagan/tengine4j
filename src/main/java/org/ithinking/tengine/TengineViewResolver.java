038625d63a3d5c27bd010b50e56c58026ef2300e
vajava@126.com <vajava@126.com>
2016-06-01 00:16:45 +0800

    完善

--- src/main/java/org/ithinking/tengine/TengineViewResolver.java
+++ src/main/java/org/ithinking/tengine/TengineViewResolver.java
@@ -9,6 +9,9 @@ import org.ithinking.tengine.loader.LoaderFactory;
 import org.springframework.web.context.support.WebApplicationObjectSupport;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.RedirectViewControllerRegistration;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.ServletContext;
 import java.util.Locale;
@@ -19,8 +22,13 @@ import java.util.Locale;
 //  org.springframework.web.servlet.view.AbstractCachingViewResolver
 public class TengineViewResolver extends WebApplicationObjectSupport implements ViewResolver, org.springframework.core.Ordered {
 
+    // org.slf4j.LoggerFactory
+    //private static final Logger vrlogger = LoggerFactory.getLogger(ThymeleafViewResolver.class);
+
     public static final String REDIRECT_URL_PREFIX = "redirect:";
     public static final String FORWARD_URL_PREFIX = "forward:";
+    private boolean redirectContextRelative = true;
+    private boolean redirectHttp10Compatible = true;
 
     private TemplateEngine manager = null;
 
@@ -92,8 +100,21 @@ public class TengineViewResolver extends WebApplicationObjectSupport implements 
     @Override
     public View resolveViewName(String viewName, Locale locale) throws Exception {
         if (!viewName.endsWith(suffix)) {
-            viewName += suffix;
+            //vrlogger.trace("[TENGINE4J] View \"{}\" cannot be handled by ThymeleafViewResolver. Passing on to the next resolver in the chain.", viewName);
+            return null;
+        } else if (viewName.startsWith(REDIRECT_URL_PREFIX)) {
+            //vrlogger.trace("[TENGINE4J] View \"{}\" is a redirect, and will not be handled directly by ThymeleafViewResolver.", viewName);
+            final String redirectUrl = viewName.substring(REDIRECT_URL_PREFIX.length());
+            final RedirectView view = new RedirectView(redirectUrl, redirectContextRelative, redirectHttp10Compatible);
+            view.setApplicationContext(getApplicationContext());
+            return view;
+        } else if (viewName.startsWith(FORWARD_URL_PREFIX)) {
+            //vrlogger.trace("[TENGINE4J] View \"{}\" is a forward, and will not be handled directly by ThymeleafViewResolver.", viewName);
+            final String forwardUrl = viewName.substring(FORWARD_URL_PREFIX.length());
+            return new InternalResourceView(forwardUrl);
         }
+
+
         Template template = manager.getTemplate(viewName, locale);
         View view = null;
         if (template != null) {
