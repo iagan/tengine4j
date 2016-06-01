e3dda08f8cc58ddad76981df0e4d46cdf4da491d
vajava@126.com <vajava@126.com>
2016-06-01 23:14:24 +0800

    完善

--- src/test/java/org/ithinking/tengine/HtmlTengineTest.java
+++ src/test/java/org/ithinking/tengine/HtmlTengineTest.java
@@ -13,114 +13,115 @@ import org.ithinking.tengine.loader.LoaderFactory;
 import org.junit.Test;
 
 public class HtmlTengineTest {
-	// SQL解析类库 SQLJEP
-	// StringTokenizer
-	/**
-	 * bug: 1. 字符区域写上 "或'字符时，会出现解析错误
-	 */
-	@Test
-	public void test() {
-		Configuration conf = Configuration.newConfiguration();
-		Loader loader = LoaderFactory.createLoader(conf);
-		HtmlParser parser = new HtmlParser();
-		//
-		User admin = new User("admin", 10, "admin@126.com", "甘肃兰州");
+    // SQL解析类库 SQLJEP
+    // StringTokenizer
 
-		List<User> userList = new ArrayList<User>();
-		userList.add(new User("阿甘", 10, "vajava@126.com", "海南海口"));
-		userList.add(new User("李四", 20, "lisi@qq.com", "美国"));
-		userList.add(new User("王五", 15, "wangwu@163.com", "中国北京"));
-		userList.add(new User("赵六", 30, "zhaoliu@xin.com", "广东广州"));
-		userList.add(new User("小明", 30, "xiaoming@xin.com", "广东广州"));
-		userList.add(new User("路人甲", 40, "lurenjia@xin.com", "广东广州"));
+    /**
+     * bug: 1. 字符区域写上 "或'字符时，会出现解析错误
+     */
+    @Test
+    public void test() {
+        Configuration conf = Configuration.newConfiguration();
+        Loader loader = LoaderFactory.createLoader(conf);
+        HtmlParser parser = new HtmlParser();
+        //
+        User admin = new User("admin", 10, "admin@126.com", "甘肃兰州");
 
-		// Context context
-		TemplateEngine manager = new TemplateEngine(loader, conf, parser);
-		Context context = new DefContext(manager);
-		Template template = context.loadTemplate("index.html");
-		context.add("title", "this is test").add("queryUrl", "http://localhost").add("userList", userList)
-				.add("admin", admin).add("showList", true);
+        List<User> userList = new ArrayList<User>();
+        userList.add(new User("阿甘", 10, "vajava@126.com", "海南海口"));
+        userList.add(new User("李四", 20, "lisi@qq.com", "美国"));
+        userList.add(new User("王五", 15, "wangwu@163.com", "中国北京"));
+        userList.add(new User("赵六", 30, "zhaoliu@xin.com", "广东广州"));
+        userList.add(new User("小明", 30, "xiaoming@xin.com", "广东广州"));
+        userList.add(new User("路人甲", 40, "lurenjia@xin.com", "广东广州"));
 
-		template.render(context);
-	}
+        // Context context
+        TemplateEngine manager = new TemplateEngine(loader, conf, parser);
+        Context context = new DefContext(manager);
+        Template template = context.loadTemplate("index.html");
+        context.add("title", "this is test").add("queryUrl", "http://localhost").add("userList", userList)
+                .add("admin", admin).add("showList", true);
 
-	@Test
-	public void testRenderPage(){
-		Configuration conf = Configuration.newConfiguration();
-		Loader loader = LoaderFactory.createLoader(conf);
-		HtmlParser parser = new HtmlParser();
-		TemplateEngine manager = new TemplateEngine(loader, conf, parser);
-		Context context = new DefContext(manager);
-		Template template = context.loadTemplate("page.html");
-		template.render(context);
-	}
+        template.render(context);
+    }
 
-	public static class User {
-		private Long id;
-		private String name;
-		private Integer age;
-		private String address;
-		private String email;
-		private boolean single;
+    @Test
+    public void testRenderPage() {
+        Configuration conf = Configuration.newConfiguration();
+        Loader loader = LoaderFactory.createLoader(conf);
+        HtmlParser parser = new HtmlParser();
+        TemplateEngine manager = new TemplateEngine(loader, conf, parser);
+        Context context = new DefContext(manager);
+        Template template = context.loadTemplate("page.html");
+        template.render(context);
+    }
 
-		public User() {
-			this("test", 25, "1233@qq.com", "广东省深圳市白石洲");
-		}
+    public static class User {
+        private Long id;
+        private String name;
+        private Integer age;
+        private String address;
+        private String email;
+        private boolean single;
 
-		public User(String name, int age, String email, String address) {
-			this.id = Math.round(Math.random() * 1000);
-			this.name = name;
-			this.age = age;
-			this.email = email;
-			this.address = address;
-		}
+        public User() {
+            this("test", 25, "1233@qq.com", "广东省深圳市白石洲");
+        }
 
-		public Long getId() {
-			return id;
-		}
+        public User(String name, int age, String email, String address) {
+            this.id = Math.round(Math.random() * 1000);
+            this.name = name;
+            this.age = age;
+            this.email = email;
+            this.address = address;
+        }
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+        public Long getId() {
+            return id;
+        }
 
-		public String getName() {
-			return name;
-		}
+        public void setId(Long id) {
+            this.id = id;
+        }
 
-		public void setName(String name) {
-			this.name = name;
-		}
+        public String getName() {
+            return name;
+        }
 
-		public Integer getAge() {
-			return age;
-		}
+        public void setName(String name) {
+            this.name = name;
+        }
 
-		public void setAge(Integer age) {
-			this.age = age;
-		}
+        public Integer getAge() {
+            return age;
+        }
 
-		public String getAddress() {
-			return address;
-		}
+        public void setAge(Integer age) {
+            this.age = age;
+        }
 
-		public void setAddress(String address) {
-			this.address = address;
-		}
+        public String getAddress() {
+            return address;
+        }
 
-		public String getEmail() {
-			return email;
-		}
+        public void setAddress(String address) {
+            this.address = address;
+        }
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+        public String getEmail() {
+            return email;
+        }
 
-		public boolean isSingle() {
-			return single;
-		}
+        public void setEmail(String email) {
+            this.email = email;
+        }
 
-		public void setSingle(boolean single) {
-			this.single = single;
-		}
-	}
+        public boolean isSingle() {
+            return single;
+        }
+
+        public void setSingle(boolean single) {
+            this.single = single;
+        }
+    }
 }
